55ba2c8e2b174da8daf8fa6efb8d48e6374578aa
Magdalena Tomaszewska <szub_m@wp.pl>
2020-05-28 10:53:46 +0200

    Version 2 HW

--- kodilla-intro/src/main/java/HelloWorld.java
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class HelloWorld {
+ public static void main(String[] args){
+    System.out.println("Hello World!");
+    System.out.println("Magda");
+    System.out.println(37);
+    System.out.println('T');
+ }
 }
