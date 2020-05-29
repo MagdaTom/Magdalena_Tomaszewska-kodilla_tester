ca18adb0453254a33ae1b3089396c2e5669981bc
Magdalena Tomaszewska <szub_m@wp.pl>
2020-05-29 10:51:13 +0200

    Initial Version zwracanie danych

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String example = sayHello();
+        System.out.println(example);
+    }
+
+    private static String sayHello() {
+        String text = "Hello from FirstClass!";
+
+        return text;
+    }
+}
