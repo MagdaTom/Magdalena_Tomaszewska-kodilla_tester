4ba287f9b1fea53a4076628f595d22c7f1cd0ca9
Magdalena Tomaszewska <szub_m@wp.pl>
2020-05-29 11:23:13 +0200

    MainExtended zmienne

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args){
+        String cityName = "Wroclaw";
+        char cityShortcut = 'W';
+        int bridges = 101;
+        double cityArea = 292.80;
+        boolean isNiceCity = true;
+        String text = "Hello!";
+        System.out.println(cityName);
+        System.out.println(cityShortcut);
+        System.out.println(bridges);
+        System.out.println(cityArea);
+        System.out.println(isNiceCity);
+        System.out.println(text);
+    }
+
+}
