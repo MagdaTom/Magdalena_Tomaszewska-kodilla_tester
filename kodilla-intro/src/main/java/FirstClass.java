61d3edd489a1ea9f9e690d99365ac6c8a44ee6ae
Magdalena Tomaszewska <szub_m@wp.pl>
2020-05-30 12:21:15 +0200

    Instrukcja if

---
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -0,0 +1,9 @@
+public class FirstClass {
+    public static void main(String[] args) {
+    System.out.println("Hello from FirstClass!");
+
+    if (1 > 2){
+        System.out.println("Niesamowite! Jeden jest większe niż dwa!!!");
+    }
+  }
+}
