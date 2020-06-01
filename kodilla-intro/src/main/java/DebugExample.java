71d9c5bb05278ab35fbb3548790f3ad754efe293
Magdalena Tomaszewska <szub_m@wp.pl>
2020-06-01 10:49:07 +0200

    Instrukcja If Else

--- kodilla-intro/src/main/java/DebugExample.java
+++ kodilla-intro/src/main/java/DebugExample.java
@@ -6,12 +6,23 @@ public class DebugExample {
         System.out.println(firstNumber);
         System.out.println(secondNumber);
 
-        sumAndDisplay(firstNumber, secondNumber);
+        if (firstNumber < secondNumber) {
+            subtractAndDisplay(firstNumber, secondNumber);
+        }
+        else {
+            sumAndDisplay(firstNumber, secondNumber);
+        }
+    }
+     private static void subtractAndDisplay(int a, int b) {
+         int result = b - a;
+         System.out.println(result);
+        }
+
+     private static void sumAndDisplay(int a, int b){
+         int result = a + b;
+         System.out.println(result);
+        }
     }
 
-    private static void sumAndDisplay(int a, int b) {
-        int result = a + b;
 
-        System.out.println(result);
-    }
-}
+
