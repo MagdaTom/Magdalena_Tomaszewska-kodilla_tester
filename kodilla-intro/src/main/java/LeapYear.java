3b4cd050bd3e5f0192688396f15722ae2e2772ac
Magdalena Tomaszewska <szub_m@wp.pl>
2020-06-01 12:04:09 +0200

    Leap Year version2

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,14 +1,11 @@
 public class LeapYear {
     public static void main(String[] args) {
-        int year = 2000;
+        int year = 1996;
 
         System.out.println(year);
 
-        if (year % 4 == 0 && year % 100 != 0) {
+        if (year % 4 == 0 && year % 100 != 0 || year%400 == 0) {
             System.out.println("Rok przestępny");
-        }
-        else if (year %4 == 0 && year%100 == 0 && year%400 == 0){
-            System.out.print("Rok przestępny");
         }
         else{
             System.out.print("Rok nie przestępny");
