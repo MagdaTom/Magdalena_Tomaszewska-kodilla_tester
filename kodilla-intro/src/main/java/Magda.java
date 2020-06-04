e00a3921e847367d90338de06120e62f1974aff3
Magdalena Tomaszewska <szub_m@wp.pl>
2020-06-04 22:12:17 +0200

    Sumowanie od 1 do 100

---
+++ kodilla-intro/src/main/java/Magda.java
@@ -0,0 +1,14 @@
+public class Magda {
+    public static void main(String[] args) {
+
+        System.out.println(suma());
+    }
+
+    private static int suma() {
+        int result = 0;
+        for (int i = 0; i <= 100; i++) {
+            result = result + (i);
+        }
+        return result;
+    }
+}
