3644a0607261bb5769b24966e0e69e09950fcf9b
Magdalena Tomaszewska <szub_m@wp.pl>
2020-06-05 18:23:40 +0200

    CwiczeniaRokPrzestepny

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,14 +1,22 @@
 public class LeapYear {
     public static void main(String[] args) {
-        int year = 1996;
+        int year = 2001;
 
         System.out.println(year);
-
-        if (year % 4 == 0 && year % 100 != 0 || year%400 == 0) {
+        if (isLeap(year))
             System.out.println("Rok przestępny");
-        }
-        else{
+        else
             System.out.print("Rok nie przestępny");
+
+    }
+
+    private static boolean isLeap(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+
+            return true;
+        } else {
+            return false;
         }
+
     }
 }
