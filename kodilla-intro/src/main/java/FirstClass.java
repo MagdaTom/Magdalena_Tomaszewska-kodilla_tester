77ae53acac2e322a2502c1d3cbe6897702ee4ec0
Magdalena Tomaszewska <szub_m@wp.pl>
2020-06-05 17:52:41 +0200

    Cwiczenia2

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,9 +1,14 @@
 public class FirstClass {
     public static void main(String[] args) {
-    System.out.println("Hello from FirstClass!");
 
-    if (1 > 2){
-        System.out.println("Niesamowite! Jeden jest większe niż dwa!!!");
+        System.out.println(suma());
     }
-  }
+
+    private static int suma() {
+        int result = 0;
+        for (int i = 0; i <= 100; i++) {
+            result = result + (i);
+        }
+        return result;
+    }
 }
--- kodilla-intro/src/main/java/Loops.java
+++ kodilla-intro/src/main/java/Loops.java
@@ -1,29 +1,6 @@
-public class Loops {
-    public static void main(String[] args){
-        int[] numbers = new int [3];
-        numbers[0] = 1;
-        numbers[1] = 2;
-        numbers[2] = 3;
-        int firstNumber = numbers[0];
-        int secondNumber = numbers[1];
-        int thirdNumber = numbers[2];
-        int numberOfElements = numbers.length;
 
 
-        int sumAll = sum(numbers);
-        System.out.println(sumAll);
 
-}
-    public static int sum(int[] numbers) {
-        int result = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            result = result + numbers[i];
-        }
-        return result;
-    }
-
-
-}
 
 
 
