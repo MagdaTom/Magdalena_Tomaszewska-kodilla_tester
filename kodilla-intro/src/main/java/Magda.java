5997cea149b24b245f21d4d910ed5465ffeea77d
Magdalena Tomaszewska <szub_m@wp.pl>
2020-06-08 14:51:08 +0200

    Temperature

--- kodilla-intro/src/main/java/Magda.java
+++ kodilla-intro/src/main/java/Magda.java
@@ -1,47 +1,19 @@
 public class Magda {
     public static void main(String[] args) {
 
-        int liczba1 = 5;
-        int liczba2 = 6;
 
-        int[] tabela = new int[3];
-        tabela[0] = 2;
-        tabela[1] = 3;
-        tabela[2] = 4;
-
-        sumAndDisplay(liczba1, liczba2);
-        System.out.println(sum(liczba1, liczba2));
-        System.out.println(subtract());
-        System.out.println(multiply());
-        System.out.println(multiply(tabela));
-
-    }
-    public static void sumAndDisplay(int a, int b){
-        int result = a + b;
-        System.out.println(result);
-    }
-    private static int sum(int a, int b){
-        return a + b;
-    }
-
-    private static int subtract(){
-        return 5 - 10;
-    }
-    private static int multiply(){
-        int result = 1;
-        for(int i = 1; i <= 4; i++){
-            result = result * i;
+        for (int i = -3; i <= 3; i++) {
+            if (i < 0)
+                System.out.println("it's cold: " + i);
+            else if (i == 0)
+                System.out.println("it's zero");
+            else
+                System.out.println("it's warm " + i);
         }
-        return result;
     }
-    private static int multiply(int[] tabela){
-        int result = 1;
-        for (int i = 0; i < tabela.length; i++){
-            result = result * tabela[i];
-        }
-        return result;
-    }
+
 }
+
 
 
 
