908c01b007c166e85b5ce9c9a672e74affed81c2
Magdalena Tomaszewska <szub_m@wp.pl>
2020-06-10 10:34:43 +0200

    Obiekt Notebook

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,14 +1,26 @@
 public class FirstClass {
     public static void main(String[] args) {
 
-        System.out.println(suma());
-    }
+        Notebook notebook = new Notebook("600g", 1000);
+        System.out.println(notebook.weight + " " + notebook.price);
+        //System.out.println(notebook.price);
 
-    private static int suma() {
-        int result = 0;
-        for (int i = 0; i <= 100; i++) {
-            result = result + (i);
-        }
-        return result;
+        Notebook heavyNotebook = new Notebook ("2000g", 500);
+        System.out.println(heavyNotebook.weight);
+        System.out.println(heavyNotebook.price);
+
+        Notebook oldNotebook = new Notebook ("1200g", 5000);
+        System.out.println(oldNotebook.weight);
+        System.out.println(oldNotebook.price);
     }
 }
+/* public class Notebook {
+    String weight;
+    int price;
+
+    public Notebook(String weight, int price){
+        this.weight = weight;
+        this.price = price;
+    }
+
+} */
