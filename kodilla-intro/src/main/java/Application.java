1a6b7b16cac1f31f288172e97693a792e163053e
Magdalena Tomaszewska <szub_m@wp.pl>
2020-06-13 12:00:49 +0200

    Ćwiczenia: refaktoryzacja kodu - correction

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -25,20 +25,3 @@ public class Application {
             userA.showAgeAndHeight();
         }
     }
-    /*TODO Review kdrzazga - Pls remove commented out code*/
-
-   /* public static void main(String[] args) {
-        String name = "Adam";
-        double age = 40.5;
-        double height = 178;
-
-        if (name != null) {
-            if (age > 30 && height > 160) {
-                System.out.println("User is older than 30 and higher then 160cm");
-            } else {
-                System.out.println("User is younger than 30 or lower than 160cm");
-            }
-        }
-    }
-}
-*/
