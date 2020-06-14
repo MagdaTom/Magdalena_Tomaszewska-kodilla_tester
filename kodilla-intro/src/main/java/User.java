4fa2397d5766190f48d8294a1c718a4f5f8debe5
Magdalena Tomaszewska <szub_m@wp.pl>
2020-06-14 21:26:39 +0200

    Pętla z wykorzystaniem obiektów(1)

---
+++ kodilla-intro/src/main/java/User.java
@@ -0,0 +1,53 @@
+
+
+public class User {
+    String name;
+    int age;
+
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public static void main(String[] args) {
+
+        User magda = new User("Magda", 37);
+        User jacek = new User("Jacek", 38);
+        User janek = new User("Janek", 7);
+        User dorota = new User("Dorota", 32);
+        User lilka = new User("Lilka", 3);
+
+        User[] users = {magda, jacek, janek, dorota, lilka};
+
+     /*   for(int i = 0; i < users.length; i++){
+            System.out.println(users[i].getName());
+        }*/
+
+
+        int result = 0;
+        for (int i = 0; i < users.length; i++) {
+            result = result + users[i].getAge();
+        }
+        int averageAge = result / users.length;
+        System.out.println(averageAge);
+
+        for (int i = 0; i < users.length; i++) {
+            if (users[i].getAge() < averageAge) {
+                System.out.println(users[i].getName());
+            }
+        }
+
+
+    }
+
+}
