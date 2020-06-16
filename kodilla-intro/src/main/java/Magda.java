847119ea38fdf38bb79b6daa211bcff397482039
Magdalena Tomaszewska <szub_m@wp.pl>
2020-06-16 14:53:00 +0200

    Metody String

--- kodilla-intro/src/main/java/Magda.java
+++ kodilla-intro/src/main/java/Magda.java
@@ -2,15 +2,26 @@
 public class Magda {
     public static void main(String[] args) {
 
+    String name = "Magda";
+    String secondName = "Magdalena      ";
+        System.out.println(name.charAt(name.length() -1));
+        System.out.println(name.charAt(2));
+        System.out.println(name.length());
+        System.out.println(name.substring(3));
+        System.out.println(name.substring(1,3));
+        System.out.println(secondName);
+        System.out.println(secondName.trim());
+        System.out.println("Ze spacjami " + secondName + " .");
+        System.out.println("Bez spacji: " + secondName.trim() + ".");
+        System.out.println(name.toUpperCase());
 
-        int liczbagwiazdek = 1;
-        for (int i = 1; i <= 5; i++) { //pętla odpowiedzialna za każdy poziom - wyrysowanie poziomu
-            for (int j = 1; j <= liczbagwiazdek; j++) {//pętla odpowiedzialna za wyrysowanie gwiazdek
-                System.out.print("*");
-            }
-            System.out.println();
-            liczbagwiazdek++;
-        }
+        System.out.println(name.startsWith("A"));
+        System.out.println(name.startsWith("Ma"));
+        System.out.println(name.startsWith("MA"));
+        System.out.println(secondName.toLowerCase().trim().startsWith("m"));
+        System.out.println(secondName.endsWith(" "));
+        System.out.println(secondName.contains("Magda"));
+        System.out.println(secondName.replace('a', 'A'));
     }
 }
 
