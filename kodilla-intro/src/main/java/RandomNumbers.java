cbf32ce62f960ef9718607618f6e558410866137
Magdalena Tomaszewska <szub_m@wp.pl>
2020-06-16 10:24:11 +0200

    Ćwiczenia: pętle/zad. 2 RandomNumbers;

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -6,9 +6,10 @@ public class RandomNumbers {
     Random random = new Random();
     List<Integer> drawnNumbers = new ArrayList<>();
 
+
     public void draw() {
         int sum = 0;
-        while (sum < 50) {
+        while (sum < 5000) {
             int number = random.nextInt(31);
             sum += number;
             drawnNumbers.add(number);
@@ -16,26 +17,36 @@ public class RandomNumbers {
 
     }
 
-    public int getMax(){
-        int max = -1000000000;
-        for (int i = 0; i < drawnNumbers.size(); i++){
-            if(drawnNumbers.get(i) > max)max = drawnNumbers.get(i);
-        }
-        return max;
-    }
-
-
     public void printDrawnNumbers() {
         for (int i = 0; i < drawnNumbers.size(); i++)
             System.out.println(drawnNumbers.get(i));
     }
+
+    public int getMax() {
+        int max = -1;
+        for (int i = 0; i < drawnNumbers.size(); i++) {
+            if (drawnNumbers.get(i) > max) max = drawnNumbers.get(i);
+        }
+        return max;
+    }
+
+    public int getMin() {
+        int min = 31;
+        for (int i = 0; i < drawnNumbers.size(); i++) {
+            if (drawnNumbers.get(i) < min) min = drawnNumbers.get(i);
+        }
+        return min;
+    }
+
 
     public static void main(String[] args) {
 
         RandomNumbers numbers = new RandomNumbers();
         numbers.draw();
         numbers.printDrawnNumbers();
-        System.out.println("it's maxnumber: " + numbers.getMax());
+        System.out.println("Max. number: " + numbers.getMax());
+        System.out.println("Min. number: " + numbers.getMin());
+
     }
 
 }
