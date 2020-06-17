f5e43cf9199e00c603e2d505bc465d8cec87aec7
Magdalena Tomaszewska <szub_m@wp.pl>
2020-06-17 11:21:09 +0200

    Dziedziczenie

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/inheritance/Application.java
@@ -0,0 +1,15 @@
+package com.kodilla.inheritance;
+
+public class Application {
+    public static void main(String[] args) {
+        Car car = new Car(4, 5);
+        car.turnOnLights();
+
+        Convertible convertible = new Convertible(4, 2);
+        convertible.turnOnLights();
+        System.out.println(convertible.getSeats());
+        convertible.displayNumberOfSeats();
+        car.displayNumberOfSeats();
+
+    }
+}
