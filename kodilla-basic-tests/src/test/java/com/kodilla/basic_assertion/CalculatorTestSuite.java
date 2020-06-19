b86ff439f8ed95f5ed202e432a2bdf60286a317f
Magdalena Tomaszewska <szub_m@wp.pl>
2020-06-19 10:43:51 +0200

    Zadanie 3.6: testy jednostkowe dla kalkulatora

---
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -0,0 +1,41 @@
+package com.kodilla.basic_assertion;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTestSuite {
+
+@Test
+    public void testSum() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int sumResult = calculator.sum(a, b);
+        assertEquals(13, sumResult);
+
+    }
+    @Test
+    public void testSubtract(){
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int subtractResult = calculator.subtract(a, b);
+        assertEquals(-3, subtractResult);
+    }
+
+    @Test
+    public void testPower(){
+        Calculator calculator = new Calculator();
+        int powResult = calculator.power(2);
+        assertEquals(4, powResult);
+        int powResult2 = calculator.power(-2);
+        assertEquals(4, powResult2);
+        int powResult3 = calculator.power(0);
+        assertEquals(0, powResult3);
+
+    }
+
+
+}
