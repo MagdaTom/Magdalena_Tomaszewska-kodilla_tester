c8dc40eb1f5bf9df1a260230ffb2bbd98bfffa42
Magdalena Tomaszewska <szub_m@wp.pl>
2020-06-24 10:02:52 +0200

    4.1 Interfaces - interfejs jako argument metody/polimorfizm

--- kodilla-collections/src/main/java/com/kodilla/collections/interfaces/InterfacesDemo.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/InterfacesDemo.java
@@ -4,13 +4,29 @@ package com.kodilla.collections.interfaces;
 public class InterfacesDemo {
     public static void main(String[] args) {
         Square square = new Square(10.0);
-        System.out.println(square.getArea());
-        System.out.println(square.getPerimeter());
+        showSquareDetails(square);
 
         Circle circle = new Circle(7.00);
+        showCircleDetails(circle);
+
+        Triangle triangle = new Triangle(6.0, 4.0, 7.211);
+        showTriangleDetails(triangle);
+
+    }
+
+    private static void showSquareDetails(Square square){
+        System.out.println(square.getArea());
+        System.out.println(square.getPerimeter());
+    }
+
+    private static void showCircleDetails(Circle circle) {
         System.out.println(circle.getArea());
         System.out.println(circle.getPerimeter());
+    }
 
+    private static void showTriangleDetails(Triangle triangle) {
+        System.out.println(triangle.getArea());
+        System.out.println(triangle.getPerimeter());
 
     }
 }
