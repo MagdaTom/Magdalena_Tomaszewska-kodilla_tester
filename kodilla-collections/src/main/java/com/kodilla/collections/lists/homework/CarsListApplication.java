50c9b77322241eb783b4086922f2242d4331f72b
Magdalena Tomaszewska <szub_m@wp.pl>
2020-06-29 09:28:25 +0200

    4.3 Zadanie: operacje na listach

--- kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
@@ -2,32 +2,36 @@ package com.kodilla.collections.lists.homework;
 
 
 import com.kodilla.collections.arrays.homework.CarUtils;
+import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Fiat;
 import com.kodilla.collections.interfaces.homework.Honda;
+import com.kodilla.collections.interfaces.homework.Opel;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class CarsListApplication {
     public static void main(String[] args) {
-        List<Honda> hondaCars = new ArrayList<>();
+        List<Car> cars = new ArrayList<>();
         Honda hondaCivic = new Honda(75);
-        hondaCars.add(hondaCivic);
-        hondaCars.add(new Honda(150));
-        hondaCars.add(new Honda(50));
-        hondaCars.add(new Honda(30));
+        cars.add(hondaCivic);
+        cars.add(new Opel(150));
+        cars.add(new Opel(50));
+        cars.add(new Honda(30));
+        cars.add(new Fiat(80));
 
 
-        for(Honda honda : hondaCars) {
-            CarUtils.describeCar(honda);
+        for(Car car : cars) {
+            CarUtils.describeCar(car);
         }
 
-        hondaCars.remove(2);
-        hondaCars.remove(hondaCivic);
+        cars.remove(2);
+        cars.remove(hondaCivic);
 
         System.out.println("Array after removal operation:");
-        for(Honda honda : hondaCars) {
-            CarUtils.describeCar(honda);
+        for(Car car : cars) {
+            CarUtils.describeCar(car);
         }
-            System.out.println("Array size after removal operation: "+ hondaCars.size());
+            System.out.println("Array size after removal operation: "+ cars.size());
     }
 }
