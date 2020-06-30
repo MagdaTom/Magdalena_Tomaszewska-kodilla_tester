28cd5882f29aafd02adba62c6f7804d9743a6d8a
Magdalena Tomaszewska <szub_m@wp.pl>
2020-06-30 12:00:45 +0200

    Thread and sleep()

---
+++ magda/src/main/java/MyFirstThread/MainThreadSleep.java
@@ -0,0 +1,42 @@
+package MyFirstThread;
+
+public class MainThreadSleep {
+    public static void main(String[] args){
+        System.out.println("Ready?");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Start");
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Threads " + i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        thread.start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Threads2 " + i);
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            }).start();
+        }
+    }
