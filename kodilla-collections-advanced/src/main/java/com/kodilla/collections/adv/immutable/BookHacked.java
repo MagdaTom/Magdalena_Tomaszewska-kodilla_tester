44fea325424f36443dc798e07cabd1ceb69c7ad6
Magdalena Tomaszewska <szub_m@wp.pl>
2020-07-01 11:57:36 +0200

    5.3 Niemutowalnosc

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/BookHacked.java
@@ -0,0 +1,18 @@
+package com.kodilla.collections.adv.immutable;
+
+//public class BookHacked extends Book{
+//    private String realTitle;
+//
+//    public BookHacked(String author, String title) {
+//        super(author, title);
+//        realTitle = title;
+//    }
+//
+//    public void modifyTitle(String newTitle){
+//        realTitle = newTitle;
+//    }
+//
+//    public String getTitle() {
+//        return realTitle;
+//    }
+//}
