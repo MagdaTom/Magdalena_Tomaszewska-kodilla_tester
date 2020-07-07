f84f37f8ccb64bed229e7157d9a6cbeba26178a9
Magdalena Tomaszewska <szub_m@wp.pl>
2020-07-07 16:04:24 +0200

    switch

---
+++ magda/src/main/java/Examples/PrintNumberInWord.java
@@ -0,0 +1,23 @@
+package Examples;
+
+public class PrintNumberInWord {
+    public static void main(String[] args) {
+
+        int number = 7;
+
+        switch(number){
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            default:
+                System.out.println("Number not found");
+
+        }
+    }
+}
