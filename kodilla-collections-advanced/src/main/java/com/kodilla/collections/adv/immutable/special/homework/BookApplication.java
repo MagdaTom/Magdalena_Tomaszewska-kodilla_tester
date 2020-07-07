b96c59470b111783f1842b5e545be6b55312ba68
Magdalena Tomaszewska <szub_m@wp.pl>
2020-07-07 12:12:50 +0200

    5.4 Zadanie: księgozbiór

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
@@ -4,9 +4,11 @@ package com.kodilla.collections.adv.immutable.special.homework;
 public class BookApplication {
     public static void main(String[] args) {
 
-        Book book1 = new BookManager().createBook("A Girl Like I", "Anita Loos");
-        Book book2 = new BookManager().createBook("Little Women", "Louisa May Alcott");
-        Book book3 = new BookManager().createBook("Little Women", "Louisa May Alcott");
+        BookManager bookManager = new BookManager();
+
+        Book book1 = bookManager.createBook("A Girl Like I", "Anita Loos");
+        Book book2 = bookManager.createBook("Little Women", "Louisa May Alcott");
+        Book book3 = bookManager.createBook("Little Women", "Louisa May Alcott");
 
         System.out.println(book1 == book2);
         System.out.println(book1 == book3);
--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
@@ -1,10 +1,22 @@
 package com.kodilla.collections.adv.immutable.special.homework;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookManager {
 
+    private List<Book> books = new ArrayList<>();
+
     public Book createBook(String title, String author) {
-        return new Book(title, author);
+        Book book1 = new Book(title, author);
+        if(books.contains(book1)){
+            System.out.println(book1 + " is a duplicate");
+        }
+        else{
+            books.add(book1);
+        }
+        return book1;
     }
 }
 
