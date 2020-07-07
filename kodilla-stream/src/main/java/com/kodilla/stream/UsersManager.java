d75a496df8391f3b3f1932f7ace55762cfaa7c6e
Magdalena Tomaszewska <szub_m@wp.pl>
2020-07-07 16:47:12 +0200

    Stream - filtrowanie strumienia

--- kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
@@ -6,6 +6,7 @@ public class UsersManager {
     public static void main(String[] args) {
         UsersRepository.getUserList()
                 .stream()
+                .filter(u -> u.getGroup().equals("Chemists"))
                 .map(UsersManager::getUserName)
                 .forEach(un -> System.out.println(un));
     }
