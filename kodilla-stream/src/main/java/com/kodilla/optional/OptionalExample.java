3270aefbea57e6ddc958156ad17c38302eae546f
Magdalena Tomaszewska <szub_m@wp.pl>
2020-07-08 10:55:39 +0200

    Optional ifPresent

--- kodilla-stream/src/main/java/com/kodilla/optional/OptionalExample.java
+++ kodilla-stream/src/main/java/com/kodilla/optional/OptionalExample.java
@@ -7,13 +7,12 @@ import java.util.Optional;
 public class OptionalExample {
     public static void main(String[] args) {
 
-        User user = null;
-        //User user = new User("user1", 30, 100, "Test");
+
+        User user = new User("user1", 30, 100, "Test");
 
         Optional<User> optionalUser = Optional.ofNullable(user);
 
-        String username = optionalUser.orElse(new User("", 0, 0, "")).getName();
-        System.out.println(username);
+        optionalUser.ifPresent(u -> System.out.println(u.getName()));
 
     }
 }
