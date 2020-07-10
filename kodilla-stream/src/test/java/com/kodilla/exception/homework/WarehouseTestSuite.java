7bb36320c2b916a14faa7b711c8304c6c7e9dc0b
Magdalena Tomaszewska <szub_m@wp.pl>
2020-07-10 10:59:55 +0200

    6.5 Zadanie: testy zamówień

---
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
@@ -0,0 +1,32 @@
+package com.kodilla.exception.homework;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WarehouseTestSuite {
+
+    @Test
+    public void testGetOrder() throws OrderDoesntExistException {
+        //given
+        Warehouse warehouse = new Warehouse();
+        warehouse.addOrder(new Order("546"));
+        Order order1 = new Order("546");
+        //when
+        Order getOrder = warehouse.getOrder("546");
+        //then
+        assertEquals(order1, getOrder);
+    }
+
+    @Test(expected = OrderDoesntExistException.class)
+    public void testGetOrder_withException() throws OrderDoesntExistException {
+        //given
+        Warehouse warehouse = new Warehouse();
+        warehouse.addOrder(new Order("546"));
+        Order order1 = new Order("546");
+        //when
+        Order getOrder = warehouse.getOrder("400");
+        //then
+        assertEquals(order1, getOrder);
+    }
+}
