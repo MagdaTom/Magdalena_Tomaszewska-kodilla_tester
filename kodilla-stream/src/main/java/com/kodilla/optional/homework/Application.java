4ac00c2160aa8d1294dc455d05054b6df11ea9ec
Magdalena Tomaszewska <szub_m@wp.pl>
2020-07-13 20:01:50 +0200

    Optional vs if

--- kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
+++ kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
@@ -11,12 +11,36 @@ public class Application {
         students.add(new Student("Sarah", null));
         students.add(new Student("Simon", new Teacher("Laura")));
         students.add(new Student("Mary", null));
+        students.add(null);
 
         for (Student student : students) {
-            System.out.println("student: " + student.getName() + ", teacher: " + Optional.ofNullable(student.getTeacher())
-                    .map(Teacher::getName)
-                    .orElse("<undefined>"));
+            System.out.print("student: ");
+            if (student != null)
+                System.out.print(student.getName());
+                else
+                System.out.print("<undefined>");
+            System.out.print(", teacher: ");
+            if (student != null)
+                if(student.getTeacher() != null)
+                    System.out.println(student.getTeacher().getName());
+                else
+                    System.out.println("<undefined>");
         }
+        System.out.println("\n----------------------------------");
+        for (Student student : students) {
+            System.out.println("student: " +
+                    Optional.ofNullable(student)
+                            .map(s -> s.getName())
+                            .orElse("<undefined>")
+                    + ", teacher: " +
+                    Optional.ofNullable(student)
+                            .map(Student::getTeacher)
+                            .map(n -> n.getName())
+                            .orElse("<undefined>"));
+        }
+//        students.stream()
+//                .map(Student::getName)
+//                .forEach(a -> System.out.println("Student: " + a));
     }
 }
 
