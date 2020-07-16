94eb5d344133174722b36105e7b1c5e5461248ff
Magdalena Tomaszewska <szub_m@wp.pl>
2020-07-16 11:18:31 +0200

    7.3 Obiekty jako dane wejściowe

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
@@ -1,8 +1,12 @@
 package com.kodilla.parametrized_tests;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,9 +20,19 @@ class StringManipulatorTestSuite {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"test,4", " OtHEr ,5", "E V e n t,5", "null ,4", "A,1"})
+    @MethodSource(value = "provideStringsForTestingLength") // mozna zostawic samo @MethodSource ale wtedy metoda musi miec takanazwe jak test
     public void shouldCalculateStringLengthWithoutSpaces(String input, int expected) {
         assertEquals(expected, manipulator.getStringLengthWithoutSpaces(input));
+    }
+
+    private static Stream<Arguments> provideStringsForTestingLength() {
+        return Stream.of(
+                Arguments.of("test", 4),
+                Arguments.of("OtHEr ", 5),
+                Arguments.of("E V e n t", 5),
+                Arguments.of("null ", 4),
+                Arguments.of("A", 1)
+        );
     }
 
     @ParameterizedTest
