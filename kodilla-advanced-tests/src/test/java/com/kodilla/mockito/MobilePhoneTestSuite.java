7dcd7a6cf236d5ae99fa057f70168a62c0cea766
Magdalena Tomaszewska <szub_m@wp.pl>
2020-07-17 14:54:21 +0200

    7.5 Mockito - wstęp

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/mockito/MobilePhoneTestSuite.java
@@ -0,0 +1,28 @@
+package com.kodilla.mockito;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+
+class MobilePhoneTestSuite {
+    MobilePhone myPhone = Mockito.mock(MobilePhone.class);
+
+    @Test
+    public void testDefaultBehaviourOfTestDouble() {
+        Assertions.assertFalse(myPhone.needsCharging());
+        Assertions.assertEquals(0.0, myPhone.getFreeStorage());
+    }
+    @Test
+    public void testExpectation() {
+        Assertions.assertFalse(myPhone.needsCharging());
+        Mockito.when(myPhone.needsCharging()).thenReturn(true);
+        Assertions.assertTrue(myPhone.needsCharging());
+    }
+    @Test
+    public void shouldCallLaunchApplication() {
+        myPhone.launchApplication("Tetris4D");
+        Mockito.verify(myPhone).launchApplication("Tetris4D");
+    }
+
+}
