a7924d76057889fc6fbc8f665166646228b7457d
Magdalena Tomaszewska <szub_m@wp.pl>
2020-07-17 10:42:07 +0200

    MethodSource

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
@@ -20,19 +20,9 @@ class StringManipulatorTestSuite {
     }
 
     @ParameterizedTest
-    @MethodSource(value = "provideStringsForTestingLength") // mozna zostawic samo @MethodSource ale wtedy metoda musi miec takanazwe jak test
+    @MethodSource(value = "com.kodilla.parametrized_tests.StringSources#provideStringsForTestingLength")
     public void shouldCalculateStringLengthWithoutSpaces(String input, int expected) {
         assertEquals(expected, manipulator.getStringLengthWithoutSpaces(input));
-    }
-
-    private static Stream<Arguments> provideStringsForTestingLength() {
-        return Stream.of(
-                Arguments.of("test", 4),
-                Arguments.of("OtHEr ", 5),
-                Arguments.of("E V e n t", 5),
-                Arguments.of("null ", 4),
-                Arguments.of("A", 1)
-        );
     }
 
     @ParameterizedTest
