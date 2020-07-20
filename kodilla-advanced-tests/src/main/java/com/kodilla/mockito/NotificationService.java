1b85a019fa5f97475937a946b02a438fa2f25895
Magdalena Tomaszewska <szub_m@wp.pl>
2020-07-20 10:15:43 +0200

    7.5 Mockito - cwiczenia/subskrypcje

--- kodilla-advanced-tests/src/main/java/com/kodilla/mockito/NotificationService.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/mockito/NotificationService.java
@@ -1,15 +1,26 @@
 package com.kodilla.mockito;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class NotificationService {
+
+    private Set<Client> clients = new HashSet<>();
 
     private Client client;
     public void addSubscriber(Client client) {
-        this.client = client;
+        this.clients.add(client);
 
     }
 
     public void sendNotification(Notification notification) {
-        this.client.receive(notification);
+        this.clients.forEach(client-> client.receive(notification));
+
+    }
+    public void removeSubscriber(Client client) {
+        this.clients.remove(client);
 
     }
 }
--- kodilla-advanced-tests/src/test/java/com/kodilla/mockito/NotificationServiceTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/mockito/NotificationServiceTestSuite.java
@@ -1,26 +1,23 @@
 package com.kodilla.mockito;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 
 public class NotificationServiceTestSuite {
+
+    NotificationService notificationService = new NotificationService();
+    Client client = Mockito.mock(Client.class);
+    Notification notification = Mockito.mock(Notification.class);
+
     @Test
     public void notSubscribedClientShouldNotReceiveNotification() {
-        NotificationService notificationService = new NotificationService();
-        Client client = Mockito.mock(Client.class);
-        Notification notification = Mockito.mock(Notification.class);
-
         notificationService.sendNotification(notification);
         Mockito.verify(client, Mockito.never()).receive(notification);
     }
 
     @Test
     public void subscribedClientShouldReceiveNotification() {
-        NotificationService notificationService = new NotificationService();
-        Client client = Mockito.mock(Client.class);
         notificationService.addSubscriber(client);
-        Notification notification = Mockito.mock(Notification.class);
 
         notificationService.sendNotification(notification);
         Mockito.verify(client, Mockito.times(1)).receive(notification);
@@ -28,16 +25,37 @@ public class NotificationServiceTestSuite {
 
     @Test
     public void notificationShouldBeSentToAllSubscribedClients() {
+        Client secondClient = Mockito.mock(Client.class);
+        Client thirdClient = Mockito.mock(Client.class);
+        notificationService.addSubscriber(client);
+        notificationService.addSubscriber(secondClient);
+        notificationService.addSubscriber(thirdClient);
 
+        notificationService.sendNotification(notification);
+        Mockito.verify(client, Mockito.times(1)).receive(notification);
+        Mockito.verify(secondClient, Mockito.times(1)).receive(notification);
+        Mockito.verify(thirdClient, Mockito.times(1)).receive(notification);
     }
 
     @Test
     public void shouldSendOnlyOneNotificationToMultiTimeSubscriber() {
+        notificationService.addSubscriber(client);
+        notificationService.addSubscriber(client);
+        notificationService.addSubscriber(client);
+
+        notificationService.sendNotification(notification);
+
+        Mockito.verify(client, Mockito.times(1)).receive(notification);
 
     }
 
     @Test
     public void unsubscribedClientShouldNotReceiveNotification() {
+        notificationService.addSubscriber(client);
+        notificationService.removeSubscriber(client);
+
+        notificationService.sendNotification(notification);
+        Mockito.verify(client, Mockito.never()).receive(notification);
 
     }
 }
