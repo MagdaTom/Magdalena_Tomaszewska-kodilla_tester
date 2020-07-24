ce14e2858f79d5dff202f0285042c40b09db88fb
Magdalena Tomaszewska <szub_m@wp.pl>
2020-07-24 12:09:17 +0200

    Ex2 - diverse

---
+++ javastart/src/main/java/var/Exercise2.java
@@ -0,0 +1,24 @@
+package var;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        double x = 10;
+        double y = 4;
+        double div = x / y;
+
+        //gdy mamy int - double div = (double)x / y;
+        System.out.println(div);
+
+        //Napisz program, który wyświetli na ekranie wyraz KOT.
+        // W programie nie możesz użyć ani jednego znaku cudzysłowu, ani apostrofów.
+
+        char k = 0x004B;
+        char o = 0x004F;
+        char t = 0x0054;
+        System.out.print(k);
+        System.out.print(o);
+        System.out.print(t);
+
+
+    }
+}
