5aa812eb0b1e8f7b62035a367afe9ef00a7618c4
Magdalena Tomaszewska <szub_m@wp.pl>
2020-07-30 12:19:15 +0200

    wsztrzykiwanie beanow metody

--- kodilla-spring-basics/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
+++ kodilla-spring-basics/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
@@ -3,15 +3,15 @@ package com.kodilla.spring.basic.spring_dependency_injection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+
 @Component
 public class SimpleApplication {
 
 //gdy nie mozna przez primary nad polem - @Resource(name = "skypeMessageService)
+    @Resource(name = "skypeMessageService")
     private MessageService messageService;
 
-    public SimpleApplication(MessageService messageService) {
-        this.messageService = messageService;
-    }
 
     public String processMessage(String message, String receiver) {
         if (checkReceiver(receiver)) {
--- kodilla-spring-basics/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SkypeMessageService.java
+++ kodilla-spring-basics/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SkypeMessageService.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
 public class SkypeMessageService implements MessageService{
     public String send(String message, String receiver) {
         return "Sending [" + message + "] to: " + receiver + " using Skype";
