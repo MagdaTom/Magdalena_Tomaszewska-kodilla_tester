a1a588d9bc43b0f34ec5511b7a71183d863ea2f5
Magdalena Tomaszewska <szub_m@wp.pl>
2020-08-11 12:37:37 +0200

    JavaStart Barman

--- javastart/src/main/java/barman/Barman.java
+++ javastart/src/main/java/barman/Barman.java
@@ -18,6 +18,15 @@ public class Barman {
     double ing2Part = drink.getIngredient2().getQuantity()/ingredientSum;
     double ing3Part = drink.getIngredient3().getQuantity()/ingredientSum;
         System.out.println("Ingredients: " + drink.getIngredient1().getName() + ", " + drink.getIngredient2().getName() + ", " + drink.getIngredient3().getName() + "; Proportions: " + ing1Part + ", " + ing2Part + ", " + ing3Part + "; Total amount: " + ingredientSum);
+   //System.out.printf("Składniki drinka to: %s, %s, %s " +
+        //                "w proporcjach %.2f, %.2f, %.2f." +
+        //                "Jego pojemność to %.0fml\n",
+        //                drink.getIngredient1().getName(), drink.getIngredient2().getName(), drink.getIngredient3().getName(),
+        //                ing1Part, ing2Part, ing3Part,
+        //                ingredientSum);
+        //%.2f oznacza liczbę zmiennoprzecinkową zaokrągloną do dwóch miejsc po przecinku,
+        //%0f oznacza liczbę zmiennoprzecinkową bez części dziesiętnej (używamy %f a nie %d, ponieważ posługujemy się typem double a nie int).
+        //%s oznacza wartość typu String
     }
 
     public static void main(String[] args) {
@@ -26,6 +35,8 @@ public class Barman {
         Barman.printDrink(drink);
         Drink drink2 = new Drink(new Ingredient("Sok ananasowy" , 100), new Ingredient("Likier malibu", 50), new Ingredient("Mleko kokosowe", 100) );
         Barman.printDrink(drink2);
+
+
     }
 
 }
