c37c3a1101ed874db86a900d2c0876c516de48a3
Magdalena Tomaszewska <szub_m@wp.pl>
2020-08-18 09:17:16 +0200

    Clock

--- kodilla-spring-basics/src/test/java/com/kodilla/spring/basic/spring_scopes/homework/ClockTestSuite.java
+++ kodilla-spring-basics/src/test/java/com/kodilla/spring/basic/spring_scopes/homework/ClockTestSuite.java
@@ -2,13 +2,11 @@ package com.kodilla.spring.basic.spring_scopes.homework;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.time.LocalDate;
+
 import java.time.LocalTime;
 
 @SpringBootTest
