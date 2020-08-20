888cca450bbc6c2f91ff9bba06dfbd707a9d495a
Magdalena Tomaszewska <szub_m@wp.pl>
2020-08-20 11:47:18 +0200

    10.4 Zadanie: ćwiczenia testów API

--- kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerTestSuite.java
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerTestSuite.java
@@ -3,29 +3,53 @@ package com.kodilla.rest.controller;
 import com.kodilla.rest.domain.BookDto;
 import com.kodilla.rest.service.BookService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 class BookControllerTestSuite {
 
+    BookService bookServiceMock = mock(BookService.class);
+    BookController bookController = new BookController(bookServiceMock);
+    List<BookDto> booksList = new ArrayList<>();
+
     @Test
     public void shouldFetchBooks(){
         //given
-        BookService bookServiceMock = Mockito.mock(BookService.class);
-        BookController bookController = new BookController(bookServiceMock);
-        List<BookDto> booksList = new ArrayList<>();
         booksList.add(new BookDto("Title1", "Author1"));
         booksList.add(new BookDto("Title2", "Author2"));
-        Mockito.when(bookServiceMock.getBooks()).thenReturn(booksList);
+        when(bookServiceMock.getBooks()).thenReturn(booksList);
         //when
         List<BookDto> result = bookController.getBooks();
         //then
         assertThat(result).hasSize(2);
     }
 
+    @Test
+    public void shouldReturnEmptyListIfnNoBooksWereAdded(){
+        //given
+        when(bookServiceMock.getBooks()).thenReturn(booksList);
+        //when
+        List<BookDto> result = bookController.getBooks();
+        //then
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    public void shouldReturnAddedBooks(){
+        //given
+        BookDto book1 = mock(BookDto.class);
+        BookDto book2 = mock(BookDto.class);
+        booksList.add(book1);
+        booksList.add(book2);
+        when(bookServiceMock.getBooks()).thenReturn(booksList);
+        //when
+        List<BookDto> result = bookController.getBooks();
+        //then
+       assertThat(result).contains(book1, book2);
+    }
 }
