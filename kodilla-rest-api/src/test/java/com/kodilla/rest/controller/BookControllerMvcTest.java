aafee42295813b8bc2f4fbe94922e6308db5bd24
Magdalena Tomaszewska <szub_m@wp.pl>
2020-08-21 14:58:16 +0200

    10.4 API Gson

--- kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerMvcTest.java
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerMvcTest.java
@@ -1,5 +1,6 @@
 package com.kodilla.rest.controller;
 
+import com.google.gson.Gson;
 import com.kodilla.rest.domain.BookDto;
 import com.kodilla.rest.service.BookService;
 import org.hamcrest.Matchers;
@@ -18,6 +19,11 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.startsWith;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
 @RunWith(SpringRunner.class)
 @WebMvcTest(BookController.class)
 public class BookControllerMvcTest {
@@ -29,7 +35,7 @@ public class BookControllerMvcTest {
     private BookService bookService;
 
     @Test
-    public void shouldFetchBooks() throws Exception{
+    public void shouldFetchBooks() throws Exception {
         //given
         List<BookDto> booksList = new ArrayList<>();
         booksList.add(new BookDto("title1", "author1"));
@@ -40,5 +46,27 @@ public class BookControllerMvcTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/books").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().is(200))
                 .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(2)));
+    }
+
+    @Test
+    public void shouldReturnAddedBooks() throws Exception {
+        //given
+        Gson gson = new Gson();
+        BookDto book = new BookDto("title1", "author1");
+        List<BookDto> booksList = new ArrayList<>();
+        booksList.add(book);
+        String bookJson = gson.toJson(book);
+
+        when(bookService.getBooks()).thenReturn(booksList);
+
+        //when & then
+        mockMvc.perform(MockMvcRequestBuilders.post("/books").contentType(MediaType.APPLICATION_JSON)
+                .content(bookJson))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("title1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.author").value("author1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.booksList").isArray())
+                .andReturn();
     }
 }
