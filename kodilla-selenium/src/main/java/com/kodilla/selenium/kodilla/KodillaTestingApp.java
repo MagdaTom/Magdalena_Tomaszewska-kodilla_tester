fbb55e5b6bf07deb55af52cbca8c87fcb12c0d31
Magdalena Tomaszewska <szub_m@wp.pl>
2020-09-10 10:52:08 +0200

    13.4 Absolute X-path

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/kodilla/KodillaTestingApp.java
@@ -0,0 +1,22 @@
+package com.kodilla.selenium.kodilla;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.Set;
+
+public class KodillaTestingApp {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\Chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://kodilla.com/pl/sign-in");
+
+        WebElement inputField = driver.findElement(By.xpath("//html/body/div/div/form/fieldset/input"));
+        inputField.sendKeys("testuser@gmail.com");
+        inputField = driver.findElement(By.xpath("//html/body/div/div/form/fieldset[2]/input"));
+        inputField.sendKeys("Haslo");
+    }
+}
