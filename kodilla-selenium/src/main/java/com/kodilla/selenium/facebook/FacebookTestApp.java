9b764688edb9989666ce7b181ac823b91fc90ca4
Magdalena Tomaszewska <szub_m@wp.pl>
2020-09-10 11:38:53 +0200

    13.4 Position X-path

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/facebook/FacebookTestApp.java
@@ -0,0 +1,21 @@
+package com.kodilla.selenium.facebook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.Set;
+
+public class FacebookTestApp {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\Chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://pl-pl.facebook.com/r.php");
+
+        WebElement yearCombo = driver.findElement(By.xpath("//select[3]"));
+        Select yearSelect = new Select(yearCombo);
+        yearSelect.selectByIndex(5);
+    }
+}
