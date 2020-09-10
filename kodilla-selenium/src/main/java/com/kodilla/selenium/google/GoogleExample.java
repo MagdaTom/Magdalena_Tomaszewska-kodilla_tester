b45d80c0151ff40298ace561facaf07b10f16f79
Magdalena Tomaszewska <szub_m@wp.pl>
2020-09-10 09:57:15 +0200

    13 Selenium google

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/google/GoogleExample.java
@@ -0,0 +1,18 @@
+package com.kodilla.selenium.google;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleExample {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\Chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+
+        WebElement inputFiled = driver.findElement(By.name( "q"));
+        inputFiled.sendKeys("Kodilla");
+        inputFiled.submit();
+    }
+}
