0a112e9b3025fba20db2cb10d0bc625f4d46fecc
Magdalena Tomaszewska <szub_m@wp.pl>
2020-09-11 11:58:05 +0200

    13.5 Zadanie: ćwiczymy selektory CSS /Modyfikacja 13.4

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -1,5 +1,4 @@
 package com.kodilla.selenium.allegro;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,7 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.List;
 
 public class AllegroTestingApp {
