606b8d9ce23db16e0ee92bcfe5cdbef9bc8a2952
Magdalena Tomaszewska <szub_m@wp.pl>
2020-09-11 11:56:55 +0200

    13.5 Zadanie: ćwiczymy selektory CSS /Modyfikacja 13.4

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -4,7 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class AllegroTestingApp {
     public static void main(String[] args) {
@@ -12,17 +16,22 @@ public class AllegroTestingApp {
         WebDriver driver = new ChromeDriver();
         driver.get("https://allegro.pl");
 
-        WebElement button = driver.findElement(By.xpath("//button[contains(@data-role, \"accept\")]"));
+        WebElement button = driver.findElement(By.cssSelector("button[data-role*='accept']"));
         button.click();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("optgroup[label='Kategorie']")));
 
-        WebElement categoryList = driver.findElement(By.xpath("//select[contains(@aria-label, \"Kategoria\")]"));
+        WebElement categoryList = driver.findElement(By.cssSelector("select[aria-label*='Kategoria']"));
         Select category = new Select(categoryList);
         category.selectByIndex(3);
 
-        WebElement searchField = driver.findElement(By.xpath("//input[@type=\"search\"]"));
+        WebElement searchField = driver.findElement(By.cssSelector("input[type='search']"));
         searchField.sendKeys("Mavic mini");
 
-        WebElement searchButton = driver.findElement(By.xpath("//button[@type=\"submit\"]"));
+        WebElement searchButton = driver.findElement(By.cssSelector("button[type='submit']"));
         searchButton.click();
+
+        List<WebElement> elements = driver.findElements(By.cssSelector("section>article"));
+        elements.forEach(element -> System.out.println(element.getText()));
     }
 }
