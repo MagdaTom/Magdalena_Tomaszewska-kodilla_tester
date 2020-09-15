d0724fd480c44868a0d82473983d11ab42762fab
Magdalena Tomaszewska <szub_m@wp.pl>
2020-09-15 12:31:21 +0200

    14.2 Cucumber

---
+++ kodilla-cucumber/src/test/java/seasonchecker/IsItSummerSteps.java
@@ -0,0 +1,40 @@
+package seasonchecker;
+
+import io.cucumber.java8.En;
+
+public class IsItSummerSteps implements En {
+
+    public IsItSummerSteps(){
+    Given("today is last day of December",() ->
+
+    {
+
+        // Write code here that turns the phrase above into concrete actions
+
+        throw new io.cucumber.java8.PendingException();
+
+    });
+
+
+    When("I ask whether it's Summer",() ->
+
+    {
+
+        // Write code here that turns the phrase above into concrete actions
+
+        throw new io.cucumber.java8.PendingException();
+
+    });
+
+
+    Then("I should be told {string}",(String string) ->
+
+    {
+
+        // Write code here that turns the phrase above into concrete actions
+
+        throw new io.cucumber.java8.PendingException();
+
+    });
+}
+}
