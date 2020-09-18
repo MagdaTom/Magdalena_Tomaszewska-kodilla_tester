c759365b620bc3ee8251f658e320b9caf236178c
Magdalena Tomaszewska <szub_m@wp.pl>
2020-09-18 13:04:07 +0200

    15.2 JaCoCo - 100% pokrycia

--- kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTestSuite.java
+++ kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTestSuite.java
@@ -8,9 +8,44 @@ class YearTestSuite {
 
     @Test
     public void shouldBeLeapYearIfDivisibleBy400() {
-
         //given
         Year year = new Year(1600);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertTrue(isLeapYear);
+    }
+
+    @Test
+    public void shouldNotBeLeapYearIfNotDivisibleBy4Or400() {
+        //given
+        Year year = new Year(1519);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertFalse(isLeapYear);
+    }
+
+    @Test
+    public void shouldNotBeLeapYearIfNotDivisibleBy400() {
+        //given
+        Year year = new Year(1000);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertFalse(isLeapYear);
+    }
+
+    @Test
+    public void shouldBeLeapYearIfDivisibleBy4ButNotBy100() {
+        //given
+        Year year = new Year(2012);
 
         //when
         boolean isLeapYear = year.isLeap();
