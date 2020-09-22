16836bb72bbff55a6a01d30be9fd48c094263fe1
Magdalena Tomaszewska <szub_m@wp.pl>
2020-09-22 13:34:08 +0200

    15.4 Hibernate

---
+++ kodilla-code-coverage/src/main/java/com/kodilla/CodeCoverageApplication.java
@@ -0,0 +1,11 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCoverageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CodeCoverageApplication.class, args);
+    }
+}
