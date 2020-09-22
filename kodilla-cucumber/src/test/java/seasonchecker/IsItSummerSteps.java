59ba362b467e3e14c2ff449122aec62f2c1c66a5
Magdalena Tomaszewska <szub_m@wp.pl>
2020-09-22 09:50:30 +0200

    Cucumber seasonchecker

--- kodilla-cucumber/src/test/java/seasonchecker/IsItSummerSteps.java
+++ kodilla-cucumber/src/test/java/seasonchecker/IsItSummerSteps.java
@@ -9,36 +9,33 @@ import java.util.Map;
 
 public class IsItSummerSteps implements En {
 
-
     private MonthDay today;
     private String answer;
-    public static Map<String, MonthDay> calender = new HashMap<>();
 
-    static {
-        calender.put("last day of December", MonthDay.of(12, 31));
-        calender.put("first day of August", MonthDay.of(8, 1));
-        calender.put("10th day of February", MonthDay.of(2, 10));
-        calender.put("20th day of June", MonthDay.of(6, 20));
-        calender.put("21st day of June", MonthDay.of(6, 21));
-    }
-
-    @Summer
     public IsItSummerSteps() {
-        Given("today is (.*) ", (String day) -> {
-            today = IsItSummerSteps.calender.get(day);
-//
-//            this.today = MonthDay.of(12, 31);
-//            this.today = MonthDay.of(8, 1);
-//            this.today = MonthDay.of(2, 10);
-//            this.today = MonthDay.of(6, 20);
-//            this.today = MonthDay.of(6, 21);
+        Given("today is last day of December", () -> {
+            this.today = MonthDay.of(12,31);
+        });
+
+        Given("today is first day of August", () -> {
+            this.today = MonthDay.of(9, 1);
+        });
+
+        Given("today is 10th day of February", () -> {
+            this.today = MonthDay.of(2, 10);
+        });
+
+        Given("today is 20th day of June", () -> {
+            this.today = MonthDay.of(6, 20);
+        });
+
+        Given("today is 21st day of June", () -> {
+            this.today = MonthDay.of(6, 21);
         });
 
         When("I ask whether it's Summer", () -> {
             SeasonChecker seasonChecker = new SeasonChecker();
-            this.answer = seasonChecker.checkIfSummer(today);
-
-
+            this.answer = seasonChecker.checkIfSummer(this.today);
         });
 
         Then("I should be told {string}", (String string) -> {
