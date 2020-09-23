8c8bfb45209daf0dd1a6e37a396e4fe183ffef90
Magdalena Tomaszewska <szub_m@wp.pl>
2020-09-23 13:54:15 +0200

    zmiana na Junit4

--- kodilla-code-coverage/src/test/java/com/kodilla/hibernate/task/repository/TaskRepositoryTestSuite.java
+++ kodilla-code-coverage/src/test/java/com/kodilla/hibernate/task/repository/TaskRepositoryTestSuite.java
@@ -2,7 +2,7 @@ package com.kodilla.hibernate.task.repository;
 
 import com.kodilla.hibernate.task.Task;
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
--- kodilla-code-coverage/src/test/java/com/kodilla/hibernate/tasklist/repository/TaskListRepositoryTestSuite.java
+++ kodilla-code-coverage/src/test/java/com/kodilla/hibernate/tasklist/repository/TaskListRepositoryTestSuite.java
@@ -2,7 +2,7 @@ package com.kodilla.hibernate.tasklist.repository;
 
 import com.kodilla.hibernate.tasklist.TaskList;
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
